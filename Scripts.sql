c5e49b12afd2c031d2b5ac7c41ca0a6be8d90fc3
Roberto Arroyo <rag@dnu.mx>
2022-06-16 16:08:35 -0500

    Modificaciones <si @ID_Padre es Empresa>

--- Scripts.sql
+++ Scripts.sql
@@ -1,7 +1,7 @@
 USE AUTO_CACAO;
 GO
 
-DECLARE @ID_PadreDeCuentahabiente INT = 0, @ID_AbueloDeCuentahabiente INT = 0;
+DECLARE @ID_PadreDeCuentahabiente INT = 0, @ID_AbueloDeCuentahabiente INT = 0, @Cuentahabiente INT = 211783;
 
 --OBTIENE EL PADRE Y ABUELO DEL CUENTAHABEINTE A NIEVEL RELACION ECONOMICA (LA RELACION PADRE-HIJO EN COLECTIVAS ES EXCLUSIVA DE RELACIONES DE JERARQUIAS)    
 SELECT  @ID_PadreDeCuentahabiente = ISNULL(PADRE.ID_ColectivaPadre, 0) ,
@@ -9,7 +9,7 @@ SELECT  @ID_PadreDeCuentahabiente = ISNULL(PADRE.ID_ColectivaPadre, 0) ,
 FROM    dbo.RelacionPadresColectivas PADRE WITH (NOLOCK)
         LEFT JOIN dbo.RelacionPadresColectivas ABUELO WITH (NOLOCK)
             ON PADRE.ID_ColectivaPadre = ABUELO.ID_Colectiva
-WHERE   PADRE.ID_Colectiva = 211785; 
+WHERE   PADRE.ID_Colectiva = @Cuentahabiente; 
 
 --SI EL PADRE O ABUELO ES NULL ENTONCES POR DEFUAL DNU ES EL PAPÁ O ABUELO O AMBAS.    
 IF @ID_PadreDeCuentahabiente IS NULL
@@ -30,6 +30,8 @@ IF @ID_AbueloDeCuentahabiente IS NULL
         WHERE   ClaveColectiva = 'CADDNU';
     END; 
 
+	SELECT @ID_PadreDeCuentahabiente AS [@ID_PadreDeCuentahabiente], @ID_AbueloDeCuentahabiente AS [@ID_AbueloDeCuentahabiente];
+
 	--NUEVAS VALIDACIONES
 IF (@ID_PadreDeCuentahabiente IS NOT NULL AND @ID_PadreDeCuentahabiente <> 0)
 	BEGIN
@@ -42,25 +44,31 @@ IF (@ID_PadreDeCuentahabiente IS NOT NULL AND @ID_PadreDeCuentahabiente <> 0)
 	--		OBTENER PADRE Y ABUELO DE @ID_PADRE
 	--		SET @PADRE = PADRE DEL @ID_PADRE
 	--		SET @ABUELO = ABUELO DEL @ID_PADRE
+	IF (SELECT	tc.ClaveTipoColectiva
+		FROM	dbo.Colectivas c 
+		LEFT	JOIN dbo.TipoColectiva tc ON tc.ID_TipoColectiva = c.ID_TipoColectiva
+		WHERE	c.ID_Colectiva IN(@ID_PadreDeCuentahabiente)) = 'CCM'
+		BEGIN
+			SELECT @ID_PadreDeCuentahabiente = ID_ColectivaPadre FROM dbo.Colectivas PADRE WITH(nolock) WHERE PADRE.ID_Colectiva = @ID_PadreDeCuentahabiente;
+			SELECT @ID_AbueloDeCuentahabiente = ID_ColectivaPadre FROM dbo.Colectivas PADRE WITH(nolock) WHERE PADRE.ID_Colectiva = @ID_PadreDeCuentahabiente;
+			SELECT @ID_PadreDeCuentahabiente AS [@ID_PadreDeCuentahabiente], @ID_AbueloDeCuentahabiente AS [@ID_AbueloDeCuentahabiente];
+		END
 	--3) SI @ID_PADRE ES CLIENTE CACAO [SESIÓN ZOOM]
 	--		OBTENER PADRE DE @ID_PADRE
 	--		SET @ABUELO = PADRE DE @ID_PADRE
-		SELECT 'DNU'
 	END
 
-SELECT @ID_PadreDeCuentahabiente AS [@ID_PadreDeCuentahabiente], @ID_AbueloDeCuentahabiente AS [@ID_AbueloDeCuentahabiente];
 
-
---QUERY QUE REVISA LOS TIPOS DE COLECTIVA
---SELECT	c.ID_Colectiva, c.ClaveColectiva, 
---		CONCAT(c.NombreORazonSocial, ' ', c.APaterno, ' ', c.AMaterno) RazonSocial,
---		CONCAT(c.ID_ColectivaPadre, ' ', c2.NombreORazonSocial, ' ', c2.APaterno, ' ', c2.AMaterno) ColectivaPadre,
---		CONCAT(c.ID_TipoColectiva, '-', tc.Descripcion) TipoColectiva
---FROM	dbo.Colectivas c 
---LEFT	JOIN dbo.TipoColectiva tc ON tc.ID_TipoColectiva = c.ID_TipoColectiva
---LEFT	JOIN dbo.Colectivas c2 ON c2.ID_Colectiva = c.ID_ColectivaPadre
---WHERE	c.ID_Colectiva IN(211784);
-
+/* QUERY QUE REVISA LOS TIPOS DE COLECTIVA
+SELECT	c.ID_Colectiva, c.ClaveColectiva, 
+		CONCAT(c.NombreORazonSocial, ' ', c.APaterno, ' ', c.AMaterno) RazonSocial,
+		CONCAT(c.ID_ColectivaPadre, ' ', c2.NombreORazonSocial, ' ', c2.APaterno, ' ', c2.AMaterno) ColectivaPadre,
+		CONCAT(c.ID_TipoColectiva, '-', tc.ClaveTipoColectiva,'-', tc.Descripcion) TipoColectiva
+FROM	dbo.Colectivas c 
+LEFT	JOIN dbo.TipoColectiva tc ON tc.ID_TipoColectiva = c.ID_TipoColectiva
+LEFT	JOIN dbo.Colectivas c2 ON c2.ID_Colectiva = c.ID_ColectivaPadre
+WHERE	c.ID_Colectiva IN(7821, 5683, 10);
+*/
 --DATOS DE PRUEBA
 --211785 TH de Subempresa
 --211783 TH de Empresa
