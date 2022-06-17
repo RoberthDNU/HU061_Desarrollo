5608706a67e3235a54296178dd689d33599d2466
Roberto Arroyo <rag@dnu.mx>
2022-06-17 18:03:14 -0500

    file: Se agrega archivo ejecucion manual de SP

--- Consultar Eventos de Polizas.sql
+++ Consultar Eventos de Polizas.sql
@@ -8,8 +8,17 @@ LEFT	JOIN dbo.Colectivas co ON co.ID_Colectiva = pd.ID_Colectiva
 LEFT	JOIN dbo.Cuentas cu ON cu.ID_Cuenta = pd.ID_Cuenta
 LEFT	JOIN dbo.TipoCuenta tc ON tc.ID_TipoCuenta = cu.ID_TipoCuenta
 LEFT	JOIN dbo.Eventos ev ON ev.ID_Evento = po.ID_Evento
-WHERE	po.ID_Operacion IN(223650)
+WHERE	po.ID_Operacion IN(	
+							SELECT	ID_Operacion 
+							FROM	dbo.Operaciones 
+							WHERE	Autorizacion IN('001870', '001871')
+						  )
 ORDER BY pd.ID_PolizaDetalle;
 
 --Retiro ATM sin los cambios Empresa-Subempresa
---SELECT * FROM dbo.Operaciones WHERE Autorizacion = '001870';
+SELECT * FROM dbo.Operaciones WHERE Autorizacion = '001871';
+
+select * from dbo.TipoCuenta where ID_TipoCuenta = 54
+select * from dbo.MediosAcceso where ID_MA = 684731 
+select * from dbo.Cuentas WHERE ID_GrupoCuenta = 1159 AND ID_ColectivaCuentahabiente = 211785
+select * from dbo.Colectivas where ID_Colectiva IN(5683, 10);
